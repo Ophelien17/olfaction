322a92ab4ba868d2c0f0885d5d0359287884507c
Ophélien Fouques <ophelien.fouques@viacesi.fr>
2021-10-19 09:00:39 +0200

    tuiles qui se retournent

--- src/components/Tuile.jsx
+++ src/components/Tuile.jsx
@@ -1,43 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
-import {withStyles} from '@material-ui/core/styles';
-import '../lol.css'
+import '../return.css'
 
-const styles = (theme) => ({
-    tuiles: {
-        textAlign: 'center',
-        borderRadius: 20,
-        backgroundColor: '#FFF',
-        cursor: 'pointer',
-        height: '100%',
-    },
-
-    imgTuile: {
-        borderRadius: '20px 20px 0 0',
-        display: 'inline-block',
-    },
-    txtTuile: {
-        borderRadius: '0 0 20px 20px',
-        height: 50,
-        backgroundColor: 'black',
-        opacity: .8,
-        textAlign: 'left',
-        padding: 5,
-        color: 'white',
-    },
-    titleTuile: {
-        textTransform: 'uppercase',
-        margin: 0,
-    },
-    subtitleTuile: {
-        margin: 0,
-    },
-
-});
 
 function Tuile(props) {
-    const {classes} = props;
     const [desc, setDesc] = useState();
     const infoOlfa = props.eachOlfa;
 
@@ -79,9 +45,6 @@ function Tuile(props) {
     );
 }
 
-Tuile.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
 
-export default withStyles(styles)(Tuile);
+export default (Tuile);
 
